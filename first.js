95a867e8e895100fb74213ad0886f5c9bf1c9662
yevginemnatsakanyan <evgine.v.mnatsakanyan@gmail.com>
2022-04-14 23:25:46 +0400

    Create first.js

---
+++ first.js
@@ -0,0 +1,8 @@
+1.Տրված է տող, տպել տողի մեջ մտնող բոլոր փոքրատառերը։
+ 
+ let str = "LLOKps"
+  for(let i = 0; i < str.length; i++){
+    if (str[i] == str[i].toLowerCase()) {
+        console.log(str[i]);
+    }
+  }
